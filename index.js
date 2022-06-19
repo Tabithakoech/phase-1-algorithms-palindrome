e76b888e5c9e2bf723860580b5398c1447b91304
TabithaKoech <tabbyk1718@gmail.com>
2022-06-19 18:02:44 +0300

    commit tree 84f97b12672da8ec0aeb035b7c627c2b179af924

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+
+function reverseString(word) {
+   const wordArray = word.split("");
+   const reversedWordArray = wordArray.reverse();
+   const reversedWord = reversedWordArray.join("");
+  return word;
 }
 
+function isPalindrome(word) {
+  const reversedWord = reverseString(word);
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+}
 /* 
   Add your pseudocode here
+Read string from the user 
+calculate the length of the string
+initialize a variable reversed to store the reversed string
+initialize i=this.length-1
+repeat until i>=0
+if string === reversed string
+  output given string is a palindrome
+else
+  output given string is not a palindrome
+stop
 */
+
 
 /*
   Add written explanation of your solution here
